5fcc97a473701f34de44aa1c27dfc1dd04b92b92
CaidynGinger <21100204@virtualwindow.co.za>
2022-09-19 12:54:53 +0200

    started header added login file

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,28 @@
 import React from 'react'
 
+import styles from './Header.module.scss'
+import openStackLogo from '../../assests/OpenStackLogo.png'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
 export const Header = () => {
+
+  // const [first, setfirst] = useState(second)
   return (
-    <div>Header</div>
+    <header className={styles.header}>
+      <div className={styles.header_nav_container}>
+      <nav  className={styles.header_nav}>
+        <img className={`${styles.logo}`} src={openStackLogo}/>
+        <Link to="/about">About</Link>
+        <Link to="/about">Teams</Link>
+        <form>
+          <span></span>
+          <input placeholder='Search...'/>
+        </form>
+        <div>Search</div>
+        <Link to="/about">Login</Link>
+      </nav>
+      </div>
+    </header>
   )
 }
