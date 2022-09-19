dd97e02fb115520727dae3678ab848e1dcf34084
CaidynGinger <21100204@virtualwindow.co.za>
2022-09-19 13:59:59 +0200

    added search function ready to be sent to server

--- client/src/components/Header/UI/SearchForm.js
+++ client/src/components/Header/UI/SearchForm.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 
 import styles from './SearchForm.module.scss'
 
 export const SearchForm = () => {
-  return (
-    <form className={styles.search_from}>
-          <span className={styles.search_from_icon}><ion-icon name="search"></ion-icon></span>
-          <input placeholder='Search...'/>
+
+    const [SearchTerm, setSearchTerm] = useState("")
+
+    const onChangeSearchTermHandler = (event) => {
+        setSearchTerm(event.target.value)
+    } 
+
+    const searchForQuestionHandler = (event) => {
+        event.preventDefault()
+        console.log("search server for " + SearchTerm);
+    }
+
+    return (
+        <form onSubmit={searchForQuestionHandler} className={styles.search_from}>
+            <span onClick={searchForQuestionHandler} className={styles.search_from_icon}><ion-icon name="search"></ion-icon></span>
+            <input value={SearchTerm} onChange={onChangeSearchTermHandler} placeholder='Search...' />
         </form>
-  )
+    )
 }
