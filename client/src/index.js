e352c681ae532c984fc239204bfa82a849b4794c
Tsebo Rams <200200@virtualwindow.co.za>
2022-09-20 09:53:44 +0200

    Removed the Register Space
    This caused the issue

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './components/Pages/Login/Login';
-import { Register } from './components/Pages/Register /Register';
+import { Register } from './components/Pages/Register/Register';
 // import App from './App';
 
 import './index.scss'
