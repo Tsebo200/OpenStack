2c2750ae3adc63c2c4ff4972d437fe35db6eea71
Tsebo Rams <200200@virtualwindow.co.za>
2022-09-20 10:34:43 +0200

    Re added previous code
    after conflict

--- client/src/components/Pages/Register/Register.jsx
+++ client/src/components/Pages/Register/Register.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import styles from './Register.module.scss';
 
-function Register() {
-    return (
-        <h1>Register Page</h1>
+export const Register = () => {
+    return(
+        <>
+            <div className={styles.register_main_container}>
+               <div className={styles.register_container}>
+                    <div className={styles.register_left}></div>
+                    <div className={styles.register_right}>
+                        <div className={styles.register_logo}></div>
+                        <p className={styles.register_create_text}>Create An Account</p>
+                        <p className={styles.register_already_text}>Already a member?</p>
+                        <p className={styles.register_signin_link}>Sign in</p>
+                        <div className={styles.register_student_number_input}></div>
+                        <div className={styles.register_username_input}></div>
+                        <div className={styles.register_email_input}></div>
+                        <div className={styles.register_password_input}></div>
+                        <div className={styles.register_confirm_password_input}></div>
+                        <button className={styles.register_create_account_btn}>Create Account</button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
-
-export default Register
