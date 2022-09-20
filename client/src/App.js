efdb035e7644e385ea9f4a0afb494f652867fa0b
Tsebo Rams <200200@virtualwindow.co.za>
2022-09-20 04:02:19 +0200

    Implemented React Helmet
    Just not functional could be some due to missing files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header/Header';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>OpenStack</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+      <meta name="description" content="A Developer's Q and A platform for The Open Window, similar to that of StackOverflow." />
+    </Helmet>
     <Header/>
     <Outlet/>
     </>
