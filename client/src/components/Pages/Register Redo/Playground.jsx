4fc9f53e42edd0a3b18a67b555a05c5b2f2578b0
Tsebo Rams <200200@virtualwindow.co.za>
2022-09-28 00:23:44 +0200

    Playground almost fully implemented
    Playground is the redo of the register page

--- client/src/components/Pages/Register Redo/Playground.jsx
+++ client/src/components/Pages/Register Redo/Playground.jsx
@@ -1,24 +1,24 @@
 import React from "react";
+import styles from './Playground.module.scss';
 
-
-const Register = () => {
+const Playground = () => {
     return(
         <>
-            <div className={"register_main_container"}>
-                <div className={"register_left"}>
-                    <div className="register_logo"></div>
-                    <h1 className="register_create_heading">Create An Account</h1>
-                    <h5 className="register_already_member">Already a Member? <a href="dashboard page">Sign in</a></h5>
+            <div className={styles.register_main_container}>
+                <div className={styles.register_left}>
+                    <div className={styles.register_logo}></div>
+                    <h1 className={styles.register_create_heading}>Create An Account</h1>
+                    <h5 className={styles.register_already_member}>Already a Member? <a href="dashboard page">Sign in</a></h5>
                     <form>
-                        <div type="number" className="reg_form_student_number"></div>
-                        <div type="text" className="reg_form_username"></div>
-                        <div type="email" className="reg_form_email"></div>
-                        <div type="password" className="reg_form_password"></div>
-                        <div type="password" className="reg_form_confirm_password"></div>
-                        <div className="reg_form_create_btn">Create Account</div>
+                        <div type="number" className={styles.reg_form_student_number}></div>
+                        <div type="text" className={styles.reg_form_username}></div>
+                        <div type="email" className={styles.reg_form_email}></div>
+                        <div type="password" className={styles.reg_form_password}></div>
+                        <div type="password" className={styles.reg_form_confirm_password}></div>
+                        <div className={styles.reg_form_create_btn}>Create Account</div>
                     </form>
                 </div>
-                    <div className={"register_right"}></div>
+                    <div className={styles.register_right}></div>
 
             </div>
         </>
@@ -26,4 +26,4 @@ const Register = () => {
 }
 // Change the divs in forms to Input with a capital i 
 // Buttpon tag isn't stylable for some reason
-export default Register
+export default Playground
