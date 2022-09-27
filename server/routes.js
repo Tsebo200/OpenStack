1dfae83919b6709130d5073e24479f3c3c29db62
Wiaan Duvenhage <wiaanduvenhage.dev@gmail.com>
2022-09-27 12:56:23 +0200

    Added user post route (WIP)

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,19 @@
 const express = require('express');;
 const router = express();
 
+router.post('/api/addUser', (req, res) => {
+    const newUser = ({
+        //add model here
+    });
+
+    newUser.save()
+    .then(i => {
+        res.json(i)
+    })
+    .catch(err => {
+        res.status(400).json({msg: "User could not be added!", err});
+    });
+    
+});
+
 module.exports = router;
