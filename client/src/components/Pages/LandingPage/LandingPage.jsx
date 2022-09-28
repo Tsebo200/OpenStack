9411a2d4e544bd324875d8d6a67b454929027e3a
CaidynGinger <91122965+CaidynGinger@users.noreply.github.com>
2022-09-28 21:10:50 +0200

    landing page v 0.1

--- client/src/components/Pages/LandingPage/LandingPage.jsx
+++ client/src/components/Pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import Input from '../../Input/Input'
+import React from "react";
+import { JoinCommunityCard } from "./JoinCommunityCard/JoinCommunityCard";
 
-import styles from './LandingPage.module.scss'
+import styles from "./LandingPage.module.scss";
+import { LecturerCard } from "./LecturerCard/LecturerCard";
 
 export const LandingPage = () => {
+  const words = ["developer", "designer"];
+  
+
   return (
     <div className={styles.landing_page}>
-    <Input></Input>
+      <section className={styles.call_to_action}>
+        <JoinCommunityCard />
+        <LecturerCard />
+      </section>
+      <section className={styles.sentence_animation}>
+        <h2>
+          Every _____ has a tab open to
+          Open stack
+        </h2>
+      </section>
     </div>
     
-  )
-}
+  );
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Login } from './components/Pages/Login/Login';
-import QuestionPortal from './components/Pages/QuestionPortal/QuestionPortal';
-import Register from './components/Pages/Register/Register';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./components/Pages/Login/Login";
+import QuestionPortal from "./components/Pages/QuestionPortal/QuestionPortal";
+import Register from "./components/Pages/Register/Register";
+import App from "./App";
 
-import './index.scss'
-import { LandingPage } from './components/Pages/LandingPage/LandingPage';
-import { ResultsPage } from './components/Pages/ResultsPage/ResultsPage';
+import { LandingPage } from "./components/Pages/LandingPage/LandingPage";
+import { ResultsPage } from "./components/Pages/ResultsPage/ResultsPage";
+import "./index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App/>} />
-            <Route path="/login" element={<Login />}/>
-            <Route path='/Register' element={<Register />}/>
-            <Route path='/question-portal' element={<QuestionPortal />}/>
-            <Route path='/resultsPage' element={< ResultsPage/>}/>
-        </Routes>
-    </BrowserRouter>
-
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/question-portal" element={<QuestionPortal />} />
+        <Route path="/resultsPage" element={<ResultsPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
-
