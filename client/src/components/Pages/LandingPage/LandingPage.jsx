8efa656da6ef2c5b6f266c5ba45e1bd4acc5608d
CaidynGinger <91122965+CaidynGinger@users.noreply.github.com>
2022-09-30 16:43:32 +0200

    almost completed registration form

--- client/src/components/Pages/LandingPage/LandingPage.jsx
+++ client/src/components/Pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { JoinCommunityCard } from "./JoinCommunityCard/JoinCommunityCard";
 
 import styles from "./LandingPage.module.scss";
@@ -6,6 +6,26 @@ import { LecturerCard } from "./LecturerCard/LecturerCard";
 
 export const LandingPage = () => {
   const words = ["developer", "designer"];
+  // const [WordCount, setWordCount] = useState(0)
+  let count = 0
+
+  const [Word, setWord] = useState(words[count])
+  
+  useEffect(() => {
+    const wordChangeTimer = setInterval(() => {
+      console.log("change word");
+      count = count + 1
+      if (count > words.length -1){
+        count = 0
+      }
+      setWord(words[count])
+    }, 5000);
+  
+    return () => {
+      clearInterval(wordChangeTimer);
+    }
+  }, [])
+  
   
 
   return (
@@ -16,7 +36,7 @@ export const LandingPage = () => {
       </section>
       <section className={styles.sentence_animation}>
         <h2>
-          Every _____ has a tab open to
+          Every <span>{Word}</span> <span className={styles.sentence_animation_word}>{Word}</span> has a tab open to
           Open stack
         </h2>
       </section>
--- client/src/components/Pages/Register Redo/Playground.jsx
+++ client/src/components/Pages/Register Redo/Playground.jsx
@@ -1,41 +1,54 @@
 import React from "react";
-import styles from './Playground.module.scss';
+import styles from "./Playground.module.scss";
 
-import backgroundImageUrl from '../../../assets/background-register.jpg'
+import backgroundImageUrl from "../../../assets/background-register.jpg";
+import formLogo from "../../../assets/OpenStackLogo.png";
+import { Input } from "../../UI/Input/Input";
+import { Button } from "../../UI/Button/Button";
 
 const Playground = () => {
-    return(
-        <div style={{backgroundImage: `url(${backgroundImageUrl})`}} className={styles.register_background}>
-            <div className={styles.register_container}>
-                <div className={`${styles.register_box} ${styles.inputs_container}`}>
-                    <div className={styles.test}>
-                        <h2>awdaw</h2>
-                    </div>
-                </div>
-                <div className={`${styles.register_box} ${styles.other}`}></div>
-            </div>
-        </div>
-        // <>
-        //     <div className={styles.register_main_container}>
-        //         <div className={styles.register_left}>
-        //             <div className={styles.register_logo}></div>
-        //             <h1 className={styles.register_create_heading}>Create An Account</h1>
-        //             <h5 className={styles.register_already_member}>Already a Member? <a href="dashboard page">Sign in</a></h5>
-        //             <form>
-        //                 <div type="number" className={styles.reg_form_student_number}></div>
-        //                 <div type="text" className={styles.reg_form_username}></div>
-        //                 <div type="email" className={styles.reg_form_email}></div>
-        //                 <div type="password" className={styles.reg_form_password}></div>
-        //                 <div type="password" className={styles.reg_form_confirm_password}></div>
-        //                 <div className={styles.reg_form_create_btn}>Create Account</div>
-        //             </form>
-        //         </div>
-        //             <div className={styles.register_right}></div>
+  return (
+    <div
+      style={{ backgroundImage: `url(${backgroundImageUrl})` }}
+      className={styles.register_background}
+    >
+      <div className={styles.register_container}>
+        <form className={`${styles.register_box} ${styles.inputs_container}`}>
+          <img className={styles.form_logo} src={formLogo}/>
+            <h4>Hello user lets get you account started</h4>
+            <hr></hr>
+            <Input label="Username" name="username" type="text"/>
+            <Input label="email" name="username" type="text"/>
+            <Input label="password" name="username" type="text"/>
+            <Input label="re-enter password" name="username" type="text"/>
+            <br/>
+            <br/>
+            <Button>Submit</Button>
+        </form>
+        <div className={`${styles.register_box} ${styles.other}`}></div>
+      </div>
+    </div>
+    // <>
+    //     <div className={styles.register_main_container}>
+    //         <div className={styles.register_left}>
+    //             <div className={styles.register_logo}></div>
+    //             <h1 className={styles.register_create_heading}>Create An Account</h1>
+    //             <h5 className={styles.register_already_member}>Already a Member? <a href="dashboard page">Sign in</a></h5>
+    //             <form>
+    //                 <div type="number" className={styles.reg_form_student_number}></div>
+    //                 <div type="text" className={styles.reg_form_username}></div>
+    //                 <div type="email" className={styles.reg_form_email}></div>
+    //                 <div type="password" className={styles.reg_form_password}></div>
+    //                 <div type="password" className={styles.reg_form_confirm_password}></div>
+    //                 <div className={styles.reg_form_create_btn}>Create Account</div>
+    //             </form>
+    //         </div>
+    //             <div className={styles.register_right}></div>
 
-        //     </div>
-        // </>
-    )
-}
-// Change the divs in forms to Input with a capital i 
+    //     </div>
+    // </>
+  );
+};
+// Change the divs in forms to Input with a capital i
 // Buttpon tag isn't stylable for some reason
-export default Playground
+export default Playground;
