f10fdbf42098003a22f347fca3d37fb64deca51a
WiaanDuvenhage-200307 <wiaanduvenhage.dev@gmail.com>
2022-10-05 11:33:36 +0200

    Added New Question Post Route

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,21 @@
 const express = require('express');;
+const questionSchema = require('./models/Questions');
 const router = express();
 
-router.post('/api/addUser', (req, res) => {
-    const newUser = ({
-        //add model here
+router.post('/api/addQuestion', (req, res) => {
+    const newQuestion = new questionSchema({
+        title: req.body.title,
+        body: req.body.body,
+        code: req.body.code,
+        screenshot: String,
+        tags: req.body.tags,
+        date: Date.now,
+        user: req.body.user,
+        answers: +req.body.answers,
+        views: +req.body.views,
+        upVotes: +req.body.upVotes,
+        downVotes: +req.body.downVotes,
+        reports: +req.body.reports
     });
 
     newUser.save()
