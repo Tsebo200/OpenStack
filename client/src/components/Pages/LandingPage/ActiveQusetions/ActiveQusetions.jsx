f4f4187f3daf64640196ac89d4d7be3424524fc4
carlokuyper <180145@virtualwindow.co.za>
2022-10-25 11:51:17 +0200

    update

--- client/src/components/Pages/LandingPage/ActiveQusetions/ActiveQusetions.jsx
+++ client/src/components/Pages/LandingPage/ActiveQusetions/ActiveQusetions.jsx
@@ -7,6 +7,7 @@ import styles from './ActiveQusetions.module.scss'
 export const ActiveQusetions = () => {
   return (
     <div  className={styles.mainActiveCon}>
+        <h3 className={styles.activeTitle}>Most active questions:</h3>
         <div className={styles.activeCon}>
           <img className={styles.activeIMG} src="https://images.unsplash.com/photo-1508317469940-e3de49ba902e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHRtbCU1Q3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
           <h4 className={styles.activeText}>How do you write html?</h4>
