ac787bb628888d4e84780dfec6223734466912e9
Tsebo Rams <200200@virtualwindow.co.za>
2022-10-26 02:21:11 +0200

    Question container done
    Questions row will just be made into a prop so more questions can be added, later considering adding a scrolling system to see more questions

--- client/src/components/Pages/UserSettings/UserSettings.jsx
+++ client/src/components/Pages/UserSettings/UserSettings.jsx
@@ -43,9 +43,12 @@ const UserSettings = () => {
 
           {/* Inside the Question Container */}
           <div className={styles.questions_container_content_box}>
+          <div className={styles.questions_row}>
           <div className={styles.question_notification_container}>
-            <h4 className={styles.question_notification_counter_text}>0</h4>
-          </div>    
+            <h4 className={styles.question_notification_counter_text}>0</h4></div> 
+          <p className={styles.question_text}>Adding custom SVGs to ion icons</p>
+          <p className={styles.date_text}>Oct 31</p>
+          </div>
           </div>
         </div> {/* End of Questions container */}
 
