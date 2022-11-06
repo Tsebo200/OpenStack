7794ec761fb10d4e6244dbff0723dde18555fee0
Tsebo Rams <200200@virtualwindow.co.za>
2022-11-06 03:02:26 +0200

    UserList
    Trying to push user profile data

--- client/src/components/Pages/UsersList/UserList.jsx
+++ client/src/components/Pages/UsersList/UserList.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
-
+import styles from "./UserList.module.scss";
+import profileIcon from "../../../assets/profilePicture.jpg";
 export const UserList = () => {
 
+// const usersInfo = [
 
+  // {
+  //   const imageLink = []
+  //   username: "Mike",
+  //   userScore: 1,
+  // }
+
+// ]
+
+// const numbers = [1, 2, 3, 4, 5];
+// const doubled = numbers.map((number) => number * 2);
+// console.log(doubled);
+
+
+let imageLink = {profileIcon}
+let username = "Mike"
+let userScore = 1
+
+const userInfo = [imageLink, username, userScore];
+
+// let userProfile = userInfo.map((imageLink));
+// console.log(userProfile);
 
 
 
   return (
-    
-    <div>UserList</div>
+    <>
+      {/* <h1>Users</h1> */}
+      <h1 className={styles.user_heading}>Users</h1>
+
+      {/* User Cards */}
+      <h2 className={styles.user_profile}>
+     {userInfo.userProfile}
+      </h2>
+      
+    </>
+
   )
 }
 
