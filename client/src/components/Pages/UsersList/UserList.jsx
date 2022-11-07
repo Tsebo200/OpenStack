0640269b9ac6994b6c3c92bcc5044b81117be583
Tsebo Rams <200200@virtualwindow.co.za>
2022-11-07 10:25:13 +0200

    Attempt two at .map()
    Really trying to push the data

--- client/src/components/Pages/UsersList/UserList.jsx
+++ client/src/components/Pages/UsersList/UserList.jsx
@@ -17,17 +17,23 @@ export const UserList = () => {
 // const doubled = numbers.map((number) => number * 2);
 // console.log(doubled);
 
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map((number) => number * 2);
+console.log(doubled);
 
-let imageLink = {profileIcon}
-let username = "Jack"
-let userScore = 50
 
-const userInfo = [imageLink, username, userScore];
+// let imageLink = {profileIcon}
+// let username = "Jack"
+// let userScore = 50
 
-// let userProfile = userInfo.map((userInfo.username));
+// const userInfo = [imageLink, username, userScore];
+
+// let userProfile = userInfo.map((userInfo));
 // console.log(userProfile);
 
-
+const actualUserInfo = [{profileIcon}, "Jack", 50];
+const UserInfo = actualUserInfo.map(actualUserInfo)
+console.log(UserInfo);
 
   return (
     <>
