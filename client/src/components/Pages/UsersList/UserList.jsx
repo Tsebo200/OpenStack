e09ef2a87edd3b415398fb06fc4fff909af0f408
CaidynGinger <91122965+CaidynGinger@users.noreply.github.com>
2022-11-07 11:17:31 +0200

    maping

--- client/src/components/Pages/UsersList/UserList.jsx
+++ client/src/components/Pages/UsersList/UserList.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
 import styles from "./UserList.module.scss";
 import profileIcon from "../../../assets/profilePicture.jpg";
 export const UserList = () => {
-
-// const usersInfo = [
+  // const usersInfo = [
 
   // {
   //   const imageLink = []
@@ -11,117 +10,50 @@ export const UserList = () => {
   //   userScore: 1,
   // }
 
-// ]
+  // ]
 
-// const numbers = [1, 2, 3, 4, 5];
-// const doubled = numbers.map((number) => number * 2);
-// console.log(doubled);
+  // const numbers = [1, 2, 3, 4, 5];
+  // const doubled = numbers.map((number) => number * 2);
+  // console.log(doubled);
 
-const numbers = [1, 2, 3, 4, 5];
-const doubled = numbers.map((number) => number * 2);
-console.log(doubled);
+  // let imageLink = {profileIcon}
+  // let username = "Jack"
+  // let userScore = 50
 
+  // const userInfo = [imageLink, username, userScore];
 
-// let imageLink = {profileIcon}
-// let username = "Jack"
-// let userScore = 50
+  // let userProfile = userInfo.map((userInfo));
+  // console.log(userProfile);
 
-// const userInfo = [imageLink, username, userScore];
+  // const actualUserInfo = [{profileIcon}, "Jack", 50];
 
-// let userProfile = userInfo.map((userInfo));
-// console.log(userProfile);
+  const actualUserInfo = [10, 11, 50];
+  const userInfo = actualUserInfo.map((number) => number * 2);
+  console.log(userInfo);
 
-
-// const actualUserInfo = [{profileIcon}, "Jack", 50];
-
-const actualUserInfo = [10, 11, 50];
-const userInfo = actualUserInfo.map((number) => number * 2);
-console.log(userInfo);
+  const numbers = [1, 2, 3, 4, 5];
 
   return (
     <>
-    <div className={styles.main_container}>
-        {/* <h1>Users</h1> */}
+      <div className={styles.main_container}>
         <div className={styles.header}>
-            <h1 className={styles.user_heading}>Users</h1>
-            <div className={styles.filter_container}></div>
-        </div>{/*End of header */}
-
-              {/* User Cards Section*/}
-              <div className={styles.all_users_container}>
-                    <div className={styles.user_card_one}>
-                      <div className={styles.user_profile_icon}></div>
-                      <h3 className={styles.user_name}>Mike</h3>
-                      <h5 className={styles.user_score}>1</h5>
-                      {/* {userInfo.userProfile} */}
-                    </div> {/* End of User_Card One */}
-
-                    {/* Start Of Card Two */}
-                    <div className={styles.hor_spacer_one}></div>
-                    <div className={styles.user_card_two}>
-                      <div className={styles.user_profile_icon}></div>
-                      <h3 className={styles.user_name}>Mike</h3>
-                      <h5 className={styles.user_score}>1</h5>
-                    </div> {/* End of User_Card_Two */}
-
-                    {/* Start Of Card Three */}
-                    <div className={styles.hor_spacer_two}></div>
-                    <div className={styles.user_card_three}>
-                      <div className={styles.user_profile_icon}></div>
-                      <h3 className={styles.user_name}>Mike</h3>
-                      <h5 className={styles.user_score}>1</h5>
-                    </div> {/* End of User_Card_Three */}
-
-                    {/* Start Of Card Four */}
-                    <div className={styles.hor_spacer_three}></div>
-                    <div className={styles.user_card_four}>
-                      <div className={styles.user_profile_icon}></div>
-                      <h3 className={styles.user_name}>Mike</h3>
-                      <h5 className={styles.user_score}>1</h5>
-                    </div> {/* End of User_Card_Four */}
-
-
-                    {/* Start Of Row 2 */}
-                    {/* Start Of Card 5 */}
-                    <div className={styles.vert_spacer_one}></div>
-                    <div className={styles.user_card_five}>
-                      <div className={styles.user_profile_icon}></div>
-                      <h3 className={styles.user_name}>Mike</h3>
-                      <h5 className={styles.user_score}>1</h5>
-                    </div> {/* End of User_Card_Five */}
-
-                    {/* Start Of Card 6 */}
-                    <div className={styles.user_card_six}>
-                      <div className={styles.user_profile_icon}></div>
-                      <h3 className={styles.user_name}>Mike</h3>
-                      <h5 className={styles.user_score}>1</h5>
-                    </div> {/* End of User_Card_Five */}
-
-                     {/* Start Of Card 7 */}
-                     <div className={styles.user_card_seven}>
-                      <div className={styles.user_profile_icon}></div>
-                      <h3 className={styles.user_name}>Mike</h3>
-                      <h5 className={styles.user_score}>1</h5>
-                    </div> {/* End of User_Card_Seven */}
-
-
-                    {/* Start Of Card 8 */}
-                    <div className={styles.user_card_eight}>
-                      <div className={styles.user_profile_icon}></div>
-                      <h3 className={styles.user_name}>Mike</h3>
-                      <h5 className={styles.user_score}>1</h5>
-                    </div> {/* End of User_Card_Eight */}
-
-          
-
-
-
-
-              </div> {/* End of users_container */}
-          
-    </div> {/* End of main-container */}
+          <h1 className={styles.user_heading}>Users</h1>
+          <div className={styles.filter_container}></div>
+        </div>
+        <div className={styles.all_users_container}>
+          {numbers.map((number) => {
+            return (
+              <div className={styles.user_card}>
+                <img src={profileIcon} />
+                <div>
+                  <h5>username</h5>
+                  <p>user score : 1</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
-
-  )
-}
-
+  );
+};
